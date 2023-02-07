3478daaf7f01f87a9e0e6e792008059c81163952
Tatyana <tatyana.volkova1984@mail.ru>
2023-02-07 21:30:40 +0500

    Напишите программу для слияния нескольких словарей в один.

---
+++ 4.py
@@ -0,0 +1,13 @@
+#Напишите программу для слияния нескольких словарей в один.
+dict_a = {1:10, 2:20}
+dict_b = {3:30, 4:40}
+dict_c = {5:50, 6:60}
+result = {}
+for d in (dict_a, dict_b, dict_c):
+    result.update(d)
+print(result)
+
+# 2 вариант
+
+result = {**dict_a, **dict_b, **dict_c}
+print(result)
