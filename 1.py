1c32fe8c6ed0616e11c00168d08ea892e0898564
Tatyana <tatyana.volkova1984@mail.ru>
2023-02-07 15:24:18 +0500

    Есть список a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]. Выведите все элементы, которые меньше 5. Несколько способов решения

--- 1.py
+++ 1.py
@@ -4,3 +4,10 @@ a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 for elem in a:
     if elem < 5:
         print(elem)
+
+# 2 вариант
+print(list(filter(lambda elem: elem < 5, a)))
+
+# 3 вариант
+
+print([elem for elem in a if elem < 5])
